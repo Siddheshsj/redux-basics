18bc3a1b964727b9332d0f617ba9f92d3f7deeb1
Siddhesh <siddhesh.shirodkar@sjinnovation.com>
2021-10-14 16:30:14 +0530

    1 function to make API call for posts & users instead of 2, lodash chain utility

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,28 @@
-import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+
+  /*
+   * ForEach cannot be used with async and await instead map and promise to be used
+   */ 
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  // or
+
+  /*
+   * Utility to combine multiple functions and get value
+   * Unless value is added for chain , result wont be sent
+   */
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
@@ -7,15 +30,15 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-// export const fetchUser = id => async dispatch => {
-//   _fetchUser(id, dispatch);
-// };
-
-// or
-export const fetchUser = id => async dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async ( id , dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-})
+};
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
